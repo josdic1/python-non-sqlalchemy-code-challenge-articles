9ce260569eac1f1d10b6995ccc7403c005839098
josdic1 <joshuadicker@gmail.com>
2025-08-21 14:29:15 -0400

    completed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,126 @@
+class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if hasattr(self, "_name"):
+            return
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+
+    def articles(self):
+        return [a for a in Article.all if a.author is self]
+
+    def magazines(self):
+        mags = []
+        for a in self.articles():
+            if a.magazine not in mags:
+                mags.append(a.magazine)
+        return mags
+
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        cats = []
+        for m in self.magazines():
+            if m.category not in cats:
+                cats.append(m.category)
+        return cats if cats else None
+
+
+class Magazine:
+    all = []
+
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+
+    def articles(self):
+        return [a for a in Article.all if a.magazine is self]
+
+    def contributors(self):
+        authors = []
+        for a in self.articles():
+            if a.author not in authors:
+                authors.append(a.author)
+        return authors
+
+    def article_titles(self):
+        titles = [a.title for a in self.articles()]
+        return titles if titles else None
+
+    def contributing_authors(self):
+        from collections import Counter
+        authors = [a.author for a in self.articles()]
+        if not authors:
+            return None
+        counts = Counter(authors)
+        many = [auth for auth, n in counts.items() if n > 2]
+        return many if many else None
+
+
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
-class Author:
-    def __init__(self, name):
-        self.name = name
+        Article.all.append(self)
 
-    def articles(self):
-        pass
+    @property
+    def title(self):
+        return self._title
 
-    def magazines(self):
-        pass
+    @title.setter
+    def title(self, value):
+        if hasattr(self, "_title"):
+            return
+        if isinstance(value, str) and 5 <= len(value) <= 50:
+            self._title = value
 
-    def add_article(self, magazine, title):
-        pass
+    @property
+    def author(self):
+        return self._author
 
-    def topic_areas(self):
-        pass
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value
 
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
+    @property
+    def magazine(self):
+        return self._magazine
 
-    def articles(self):
-        pass
-
-    def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine):
+            self._magazine = value
